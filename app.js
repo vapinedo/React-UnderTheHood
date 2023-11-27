57ccd361c0b70a8c221df771afef0785e8f1a65d
Victor Pinedo <victor.pineda@linea.ai>
2023-11-27 18:23:35 -0500

    The DOM and declarative programming

--- app.js
+++ app.js
@@ -1,35 +1,19 @@
-var list = document.getElementById("list");
-var boton = document.getElementById("button");
-
-function agregarLi() {
-    var nuevoLi = document.createElement("li");
-    nuevoLi.innerText = "Item 4";
-    list.appendChild(nuevoLi);
+const countApp = {
+    getCount: () => {
+        const countElement = document.getElementById("count");
+        return Number(countElement.textContent);
+    },
+    setCount: (val) => {
+        const countElement = document.getElementById("count");
+        countElement.textContent = val;
+    }
 };
 
-boton.addEventListener("click", agregarLi);
-
-var listaDeNumeros = [4, 5, 8, 7, 10];
-
-function sacarParesEImpares(arrayDeNumeros) {
-    var pares = [];
-    var impares = [];
-
-    for (var iterar = 0; iterar < arrayDeNumeros.length; iterar++) {
-        var numero = arrayDeNumeros[iterar];
-        if (numero % 2 === 0) {
-            pares.push(numero);
-        } else {
-            impares.push(numero);
-        }
+function setCount() {
+    let count = countApp.getCount();
+    if (count >= 5) {
+        countApp.setCount(0);
+    } else {
+        countApp.setCount(count + 1);
     }
-    return { pares, impares };
 }
-
-var { pares, impares } = sacarParesEImpares(listaDeNumeros);
-console.log(pares);
-console.log(impares);
-
-
-var nuevoArray = [11, 20];
-console.log(sacarParesEImpares(nuevoArray));
