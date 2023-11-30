99fbbce226c1cf3f6f512b456121c0de24e461b2
Victor Pinedo <victor.pineda@linea.ai>
2023-11-30 10:18:48 -0500

    React element trees

--- creating-react-elements/app.js
+++ creating-react-elements/app.js
@@ -3,6 +3,12 @@ const root = ReactDOM.createRoot(rootNode);
 root.render(React.createElement(App));
 
 function App() {
-    return React.createElement("button", null, "Click me");
+    return (
+        React.createElement("article", null,
+            React.createElement("h2", null, "Counter"),
+            React.createElement("p", null, "You clicked 1 times"),
+            React.createElement("button", null, "Click me"))
+    );
 }
-console.log(React.createElement(App));
+
+console.log(App());
